105cfd842120e2d245b17c0f091bb21bf546aeb6
Sahaana15 <ansahaana@gmail.com>
2026-09-04 22:10:44 +0530

    Add session 2 String Operations & Performance class problems

--- src/stringoperationsandperformance/class_problems/CSVStudentRecordParser.java
+++ src/stringoperationsandperformance/class_problems/CSVStudentRecordParser.java
@@ -1,4 +1,24 @@
 package stringoperationsandperformance.class_problems;
-
 public class CSVStudentRecordParser {
+
+    void parseStudentRecord(String csvLine) {
+        String[] fields = csvLine.split(",");
+
+        if (fields.length != 3) {
+            System.out.println("Invalid Record");
+            return;
+        }
+
+        System.out.println("Name: " + fields[0] +
+                " | Roll No: " + fields[1] +
+                " | Dept: " + fields[2]);
+    }
+
+    public static void main(String[] args) {
+        String csvLine = "Ananya Verma,RA2211003010123,CSE";
+
+        CSVStudentRecordParser obj = new CSVStudentRecordParser();
+        obj.parseStudentRecord(csvLine);
+    }
 }
+
